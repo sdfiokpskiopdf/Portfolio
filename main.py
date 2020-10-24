c54def25da569d6180f54d02660ebe8917edddb0
faizanshah-python <73015259+faizanshah-python@users.noreply.github.com>
2020-10-24 21:02:45 +0100

    set up clean navbar

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+from flask import Flask, render_template, url_for
 import os
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
