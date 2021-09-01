0fc7302fad8a2babfa75f1ceb1bd208a45141287
Your Name <faizanshah123123123@gmail.com>
2021-09-01 18:00:49 +0100

    projects page

--- main.py
+++ main.py
@@ -3,39 +3,49 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/home')
-@app.route('/')
+
+@app.route("/home")
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/skills')
+
+@app.route("/skills")
 def skills():
-    return render_template('skills.html')
+    return render_template("skills.html")
 
-@app.route('/projects')
+
+@app.route("/projects")
 def projects():
-    return render_template('projects.html')
+    return render_template("projects.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
+
+# Dynamically named static files
+# Solves problems related to browser cache
+# /static/css/style.css?q=1280549780
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
 
+
 def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
+    if endpoint == "static":
+        filename = values.get("filename", None)
         if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values["q"] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=4000)
